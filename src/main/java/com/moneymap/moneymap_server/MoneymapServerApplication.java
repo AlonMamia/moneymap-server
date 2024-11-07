40c9f832cfb8f3911dc0550926036edeff96c7e8
AlonMamia <alon5585@gmail.com>
2024-11-07 22:39:18 +0200

    first

---
+++ src/main/java/com/moneymap/moneymap_server/MoneymapServerApplication.java
@@ -0,0 +1,13 @@
+package com.moneymap.moneymap_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymapServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moneymap/moneymap_server/MoneymapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneymap.moneymap_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
